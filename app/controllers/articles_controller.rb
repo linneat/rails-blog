0e98aed076457f14ba7a7db697059d73aade1554
linneat <51674082+linneat@users.noreply.github.com>
2020-07-23 15:38:04 +0200

    editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(article_params)
  
@@ -20,6 +24,16 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end
 
     private
     def article_params
