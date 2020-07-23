522e96f242fb95b2562615c0f8d3122203a55032
linneat <51674082+linneat@users.noreply.github.com>
2020-07-23 15:13:26 +0200

    adding links

--- db/migrate/20200722103128_create_articles.rb
+++ db/migrate/20200722103128_create_articles.rb
@@ -2,7 +2,6 @@ class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :string
       t.text :text
 
       t.timestamps
