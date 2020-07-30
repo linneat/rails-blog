d5ca58be89c27b940b34ce727353009c6c995350
linneat <51674082+linneat@users.noreply.github.com>
2020-07-30 11:42:51 +0200

    respond to json on index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
     def index
         @articles = Article.all
+       
+        respond_to do |format|
+          format.html
+          format.json { render json: @articles }
+        end
     end
 
     def show
